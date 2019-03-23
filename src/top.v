efadef00d8a10a26b3bd37bc8e75cab82bbf1698
George Rennie <georgerennie@gmail.com>
2019-03-23 21:53:33 +0000

    added alu demo

--- src/top.v
+++ src/top.v
@@ -3,6 +3,8 @@
 //Quickly include files from the George Rennie Verilog Modules Library:
 `define GRVM_PATH(module_file_address) "../../Shared Files/Verilog/George-Rennie-Verilog-Modules/module_file_address"
 `include `GRVM_PATH(synchronous/reset_conditioner.v)
+`include `GRVM_PATH(interfacing/tri_state_buffer.v)
+`include `GRVM_PATH(synchronous/binary_counter.v)
 
 `include "alu.v"
 
@@ -40,5 +42,45 @@ module top(
         .clk(clk),
         .in(io_button),
         .out(button_pd_out));
+
+    wire [7 : 0] main_bus;
+    always @* begin
+      io_led[7 : 0] = main_bus;
+    end
+
+    wire slow_clk;
+    binary_counter #(.SIZE(32)) clk_count (
+        .clk(clk),
+        .rst(rst),
+        .top(32'hFFFFFFFF)
+    );
+
+    assign slow_clk = ~clk_count.out[27];
+    //assign slow_clk = clk;
+
+    tri_state_buffer test_input [7 : 0] (
+        .in(dip_pd_out[7 : 0]),
+        .enable(dip_pd_out[23]),
+        .out(main_bus)
+    );
+
+    alu alu_inst(
+        .bus(main_bus),
+        .clk(slow_clk),
+        .clr(rst),
+
+        .load_A(dip_pd_out[22]),
+        .load_B(dip_pd_out[21]),
+        .write_A(dip_pd_out[20]),
+        .write_B(dip_pd_out[19]),
+
+        .write_ALU(dip_pd_out[18]),
+        .subtract(dip_pd_out[17])
+    );
+
+    always @* begin
+        io_led [9 : 8] = {alu_inst.zero, alu_inst.carry};
+        led[0] = slow_clk;
+    end
 
 endmodule
